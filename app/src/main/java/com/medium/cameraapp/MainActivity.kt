48d4dacefa36efc4707793c171a0f8ea2a387bb8
Giga99 <igorstevanovic99@gmail.com>
2022-09-13 11:08:34 +0200

    Added MainScreen and navigation

--- app/src/main/java/com/medium/cameraapp/MainActivity.kt
+++ app/src/main/java/com/medium/cameraapp/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.medium.cameraapp.ui.theme.CameraAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,12 +17,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CameraAppTheme {
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.MAIN
+                    ) {
+                        composable(Route.MAIN) {
+                            MainScreen(navController)
+                        }
+
+                        composable(Route.IMAGE) {
+
+                        }
+
+                        composable(Route.VIDEO) {
+
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+object Route {
+    const val MAIN = "main"
+    const val IMAGE = "image"
+    const val VIDEO = "video"
 }
