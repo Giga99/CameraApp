8700ad8f5dcd02ea8131cd2fc41c20001b5d76ec
Giga99 <igorstevanovic99@gmail.com>
2022-09-13 22:49:15 +0200

    Refactor

--- app/src/main/java/com/medium/cameraapp/VideoPreviewScreen.kt
+++ app/src/main/java/com/medium/cameraapp/VideoPreviewScreen.kt
@@ -1,6 +1,5 @@
 package com.medium.cameraapp
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
